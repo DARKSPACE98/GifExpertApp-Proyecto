0ce257e8423f4cfa581e13649d44bc368895e995
DARKSPACE98 <enriquechuyin2019@gmail.com>
2023-02-20 22:10:51 -0700

    GifExpertApp en react completada

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import { AddCategory, GifGrid } from "./components";
+
+export const GifExpertApp = () => {
+
+  const [categories, setCategories] = useState(['One punch']);
+
+  const onAddCategory = (newCategory) => {{
+
+    if (categories.includes(newCategory)) return;
+    
+   // console.log(newCategory);
+
+    //categories.push(newCategory);
+    //SPREAD ES PARA CREAR COPIAS DE UN ARREGLO
+    setCategories( [newCategory,...categories] )
+  
+  }
+
+  }
+
+  return (
+    <>
+        <h1>GifExpertApp</h1>
+    <AddCategory 
+    onNewCategory={ (value) => onAddCategory(value)} 
+    />
+      {
+        categories.map(( category ) => 
+        (<GifGrid 
+          key={category}
+          category={category}/>)
+        )
+      }     
+    </>
+  )
+}
