332331afeefa7500d4b9b472c91a2fe72fcfc0fc
DARKSPACE98 <enriquechuyin2019@gmail.com>
2023-02-24 14:55:52 -0700

    Pruebas en el Helper getGifs

--- test/components/GifItem.test.js
+++ test/components/GifItem.test.js
@@ -1,21 +1,35 @@
 import { GifItem } from "../../src/components/GifItem";
-import React, { Component } from 'react';
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 
 describe('Pruebas en el GifItem', () => { 
     const title = 'Saitama';
     const url = 'https://one-punch.com/saitama.jpg';
 
-    test('Titulo debe ser obligatorio', () => {
+    test('Debe de hacer match con el snapshot', () => {
         
-        const { container } = render(<GifItem title={title} />)
+        const { container } = render(<GifItem title={title} url={url} />)
         expect(container).toMatchSnapshot();
+    
      })
+     test('Debe de mostrar la imagen con el url y el alt indicado', () => {
 
-     test('Url debe ser obligatorio', () => {
-        
-        const { container } = render(<GifItem url={url} />)
-        expect(container).toMatchSnapshot();
-     })
-     
+        render(<GifItem title={title} url={url} />)
+        //screen.debug();
+        //expect( screen.getByRole('img').scr)
+        /* console.log( screen.getByRole('img').scr).toBe(url);
+        console.log( screen.getByRole('img').alt).toBe(title); */
+        const { src, alt } = screen.getByRole('img');
+        expect( src ).toBe(url);
+        expect( alt ).toBe(title);
+
+
+      })
+
+      test('debe de mostrar el titulo en el componente', () => { 
+
+        render(<GifItem title={title} url={url} />);
+        expect( screen.getByText(title)).toBeTruthy();
+
+       })
+
  })
